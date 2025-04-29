4f32955ee602f0f7ad55dbb4932b0ce17cf37ea5
nejvad <jenesesdavid620@gmail.com>
2025-04-29 03:15:40 +0200

    movie mapping

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,17 @@ const App = () => {
 
         <section className='all-movies'>
           <h2>All Movies</h2>
-          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+          {isLoading ? (
+            <p className="text-white">Loading...</p>
+          ): errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ): 
+            <ul>
+              {movies.map((movie) => (
+                <p className='text-white'>{movie.title}</p>
+              ))}
+            </ul>
+          }
         </section>
       </div>
     </main>
