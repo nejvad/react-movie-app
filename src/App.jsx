e7d03f41a3b5a41ff7df07b0436efbe685a50fa8
nejvad <jenesesdavid620@gmail.com>
2025-04-29 02:16:24 +0200

    search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Search from './components/Search'
 
 const App = () => {
   return (
@@ -9,6 +10,8 @@ const App = () => {
           <img src="./hero.png" alt="Hero Banner"/>
           <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
         </header>
+
+        <Search />
       </div>
     </main>
   )
